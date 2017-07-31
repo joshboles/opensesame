0b9af87e19dbd7d2ff5ba04d587c89e79b4fe3f2
Josh Boles <jaboles@gmail.com>
2017-07-30 22:06:50 -0400

    Initial-ish commit

---
+++ opensesame/ViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  opensesame
+//
+//  Created by Josh Boles on 7/30/17.
+//  Copyright © 2017 Josh Boles. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var garageButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        updateUI()
+    }
+    
+    func updateUI() {
+        if checkGarageStatus() == true {
+            let image = UIImage(named: "garage-open") as UIImage?
+            garageButton.setImage(image, for: .normal)
+        } else {
+            let image = UIImage(named: "garage-closed") as UIImage?
+            garageButton.setImage(image, for: .normal)
+        }
+    }
+    
+    func checkGarageStatus() -> Bool {
+        // TODO: Eventually make API call to Smarthings and return if garage is actually open or not.
+        return true
+    }
+    
+    func signalGarage() {
+        // Make REQUEST here to http://192.168.1.??:??
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
